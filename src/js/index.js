6f52bc90a153eee191ed35e21d309f0096fd84a0
Tobias <tobiasramossilva@gmail.com>
2023-03-18 21:31:15 -0300

    Manipulando as teclas de números junto com a de vírgula

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,25 @@ const ton = document.getElementById("ton")
 const tclean = document.getElementById("tclean")
 const tresponse = document.getElementById("tresponse")
 
+let signal = false
+let decimal = false
+
+keysNum.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        if (evt.target.innerHTNL == ",") {
+            if (!decimal) {
+                decimal = true
+                if (display.innerHTML == "0") {
+                    display.innerHTML = "0,"
+                } else {
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        } else {
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
+    })
+})
