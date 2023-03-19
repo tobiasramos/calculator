615f3ae8074d0ff576e8b3ba5fd5016e94f7314f
Tobias <tobiasramossilva@gmail.com>
2023-03-18 21:13:54 -0300

    Pegando os elementos que vou utilizar para fazer a manipulação deles

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const keysNum = [...document.querySelectorAll(".num")]
+const keysOp = [...document.querySelectorAll(".op")]
+const keyRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const ton = document.getElementById("ton")
+const tclean = document.getElementById("tclean")
+const tresponse = document.getElementById("tresponse")
+
